204b4bd42661831b23a098173269b7a9ebf52431
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-02-18 03:02:01 +0300

    simplified application.mk: most of the logic moved to the top level makefile

--- application.mk
+++ application.mk
@@ -11,37 +11,6 @@ PGM ?= ${ARCH}/$(EXEC)
 MANAGERS ?= all
 
 #
-# Search for source files if the top-level makefile doesn't specify them explicitly
-#
-ifdef SRC_DIR
-  ifeq ($(and $(CSRCS),$(CXXSRCS)),)
-    CSRCS = $(wildcard $(SRC_DIR)/*.c)        \
-            $(wildcard $(SRC_DIR)/*/*.c)      \
-            $(wildcard $(SRC_DIR)/*/*/*.c)    \
-            $(wildcard $(SRC_DIR)/*/*/*/*.c)  \
-            $(wildcard $(SRC_DIR)/*/*/*/*/*.c)
-
-    CXXSRCS = $(wildcard $(SRC_DIR)/*.cpp)        \
-              $(wildcard $(SRC_DIR)/*/*.cpp)      \
-              $(wildcard $(SRC_DIR)/*/*/*.cpp)    \
-              $(wildcard $(SRC_DIR)/*/*/*/*.cpp)  \
-              $(wildcard $(SRC_DIR)/*/*/*/*/*.cpp)
-  endif
-endif
-
-#
-# Common compiler flags
-# Application specific flags are appended so the default flags can be overriden
-#
-common_flags = -Wall -Wextra -Wundef -Wfloat-equal -Wmissing-declarations -pedantic \
-               -ffunction-sections -fdata-sections
-
-AM_CFLAGS := $(common_flags) -std=c99 $(AM_CFLAGS)
-AM_CXXFLAGS := $(common_flags) -std=c++11 $(AM_CXXFLAGS)
-
-AM_LDFLAGS := -Wl,--gc-sections $(AM_LDFLAGS)
-
-#
 # RTEMS makefiles
 #
 ifndef RTEMS_MAKEFILE_PATH
